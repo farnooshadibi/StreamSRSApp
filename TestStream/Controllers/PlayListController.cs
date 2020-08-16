ec5157ad71085aa6a84a48fbc8b9b1b71d504687
Farnoosh Adibi <f.adibi@AminMVNO.local>
2020-08-16 14:44:12 +0430

    createPlayList

---
+++ TestStream/Controllers/PlayListController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestStream.Data;
+using TestStream.Extra_Classes;
+using TestStream.Models;
+
+namespace TestStream.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayListController : ControllerBase
+    {
+        private ApplicationDbContext db;
+        public PlayListController(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                var playList = db.playLists.Where(c => c.IsActive == true).ToList();
+                Response response = new Response();
+                response.Data = playList;
+                response.Status = true;
+                response.Message = "Received successfully";
+                return Ok(response);
+
+            }
+            catch (Exception e)
+            {
+                writeException.Write(e.Message, DateTime.Now, "PlayList", "Get", "Admin");
+                return this.NotFound("Dosnt Received successfully");
+            }
+
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult Post([FromBody] PlayList playList)
+        {
+            try
+            {
+                Response response = new Response();
+
+
+                db.playLists.Add(playList);
+                db.SaveChanges();
+                response.Data = playList;
+                response.Status = true;
+                response.Message = " Create successfully";
+
+
+                return Ok(response);
+            }
+
+            catch (Exception e)
+            {
+                writeException.Write(e.Message, DateTime.Now, "PlayList", "Post", "Admin");
+                return this.NotFound("Dosnt Create successfully");
+            }
+        }
+    }
+}
