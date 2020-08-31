4543c2eccd01c72bf6573cfa6fbb40adfc8a4340
f.adibi <f.adibi@VDI-DEV-2>
2020-08-31 09:08:32 +0430

    convertDate

---
+++ TestStream/Extra_Classes/ConvertDate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestStream.Extra_Classes
+{
+    public static class ConvertDate
+    {
+        /// <summary>
+        /// یک استرینگ تاریخ شمسی را به معادل میلادی تبدیل میکند
+        /// </summary>
+        /// <param name="persianDate">تاریخ شمسی</param>
+        /// <returns>تاریخ میلادی</returns>
+        public static DateTime ToGeorgianDateTime(this string persianDate)
+        {
+            int year = Convert.ToInt32(persianDate.Substring(0, 4));
+            int month = Convert.ToInt32(persianDate.Substring(5, 2));
+            int day = Convert.ToInt32(persianDate.Substring(8, 2));
+            DateTime georgianDateTime = new DateTime(year, month, day, new System.Globalization.PersianCalendar());
+            return georgianDateTime;
+        }
+
+
+
+        /// <summary>
+        /// یک تاریخ میلادی را به معادل فارسی آن تبدیل میکند
+        /// </summary>
+        /// <param name="georgianDate">تاریخ میلادی</param>
+        /// <returns>تاریخ شمسی</returns>
+        public static string ToPersianDateString(this DateTime georgianDate)
+        {
+            System.Globalization.PersianCalendar persianCalendar = new System.Globalization.PersianCalendar();
+
+            string year = persianCalendar.GetYear(georgianDate).ToString();
+            string month = persianCalendar.GetMonth(georgianDate).ToString().PadLeft(2, '0');
+            string day = persianCalendar.GetDayOfMonth(georgianDate).ToString().PadLeft(2, '0');
+            string persianDateString = string.Format("{0}/{1}/{2}", year, month, day);
+            return persianDateString;
+        }
+
+    }
+}
